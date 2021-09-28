f2dc862b4b9880ceb141fa784c4b38f575937017
Anton Kossler <a.kossler@digipen.edu>
2021-09-28 15:31:24 -0700

    Created a Tk test window

---
+++ __main__.py
@@ -0,0 +1,20 @@
+import tkinter
+
+class Application(tkinter.Frame):
+    def __init__(self, title, master=None):
+        tkinter.Frame.__init__(self, master)
+        self.master.title(title)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.sampletext = tkinter.Label(self, text="This is a test window.")
+        self.sampletext.grid()
+
+def main():
+    app = Application("Test")
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
